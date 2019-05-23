c4e463eb4e8e69d07f70c57edc839baa64b02f81
nacho <nama1510@gmail.com>
2019-05-23 21:39:05 +0200

    03 - Sending Response from the server

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.nethod, req.headers);
   //process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write('<html>');
+  res.write('<head><title> My First Page </title></head>');
+  res.write('<body><h1> Hello from my first NodeJS Server </h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 //Listen on port 3000
