70c440d83c87d011d4ad408947ed043b1a643ea6
nacho <nama1510@gmail.com>
2019-05-23 21:31:23 +0200

    02 - Understanding Requests

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require("http");
 
+//Create Server
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.nethod, req.headers);
+  //process.exit();
 });
 
+//Listen on port 3000
 server.listen(3000);
