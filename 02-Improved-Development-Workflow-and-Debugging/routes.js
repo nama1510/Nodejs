238a76d0df36ac6696890bd04f488c16e1675641
nacho <nama1510@gmail.com>
2019-05-27 17:08:45 +0200

    10 - Restarting the debugger

--- 02-Improved-Development-Workflow-and-Debugging/routes.js
+++ 02-Improved-Development-Workflow-and-Debugging/routes.js
@@ -30,11 +30,11 @@ const requestHandler = (req, res) => {
       }); //We write the data into the file
     });
   }
-  // res.setHeader("Content-Type", "text/html");
-  // res.write("<html>");
-  // res.write("<head><title>My Firtst Page</title></head>");
-  // res.write("<body><h1>Hello from my NodeJS server</h1></body>");
-  // res.write("</html>");
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My Firtst Page</title></head>");
+  res.write("<body><h1>Hello from my NodeJS server</h1></body>");
+  res.write("</html>");
 };
 
 module.exports = {
