f55857103fe3e90f29a1486e5af8e18cc6e5f47f
nacho <nama1510@gmail.com>
2019-05-27 19:03:58 +0200

    15 - extracting code to routes files

--- 03-Working-with-ExpressJS/app.js
+++ 03-Working-with-ExpressJS/app.js
@@ -3,22 +3,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  ); //send a response
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from express</h1>"); //send a response
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 //Create Server
 app.listen(3000);
