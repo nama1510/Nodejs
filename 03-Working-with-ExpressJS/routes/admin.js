09c2edd7d2f48380c26c70db95f1e202d1854508
nacho <nama1510@gmail.com>
2019-05-27 20:48:48 +0200

    19 - Sending html files

--- 03-Working-with-ExpressJS/routes/admin.js
+++ 03-Working-with-ExpressJS/routes/admin.js
@@ -1,10 +1,13 @@
+const path = require("path");
 const express = require("express");
 const router = express.Router(); //kinda like a mini express app
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  ); //send a response
+  // __dirname: the directory in which the current file is located
+  // ../: go up one level
+  // views: views folder
+  // add-product.html: the page we want to link
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post("/add-product", (req, res, next) => {
