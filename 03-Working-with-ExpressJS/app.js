a45b4c663d079bdcd5cf2fa33253d666b70f1ed9
nacho <nama1510@gmail.com>
2019-05-27 18:17:02 +0200

    11 - Using ExpressJS

---
+++ 03-Working-with-ExpressJS/app.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In another the middleware");
+  res.send("<h1>The Add Product page</h1>"); //send a response
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In another the middleware");
+  res.send("<h1>Hello from express</h1>"); //send a response
+});
+
+//Create Server
+app.listen(3000);
