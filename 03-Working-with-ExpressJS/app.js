ee7cf24ff9fe90989d05f91a0fcc9cc27a889a14
nacho <nama1510@gmail.com>
2019-05-27 18:46:40 +0200

    13 - Handling POST requests

--- 03-Working-with-ExpressJS/app.js
+++ 03-Working-with-ExpressJS/app.js
@@ -1,19 +1,22 @@
-const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("This always runs");
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/add-product", (req, res, next) => {
-  res.send("<h1>The Add Product page</h1>"); //send a response
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  ); //send a response
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another the middleware");
   res.send("<h1>Hello from express</h1>"); //send a response
 });
 
