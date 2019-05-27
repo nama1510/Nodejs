9536ed66f4ca50ff5ec9307b7d3613f5846e2a11
nacho <nama1510@gmail.com>
2019-05-27 20:31:56 +0200

    17 - Filtering Paths

--- 03-Working-with-ExpressJS/routes/admin.js
+++ 03-Working-with-ExpressJS/routes/admin.js
@@ -1,14 +1,13 @@
 const express = require("express");
-
 const router = express.Router(); //kinda like a mini express app
 
 router.get("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   ); //send a response
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
