4b8dd9f2b853ae7159616132b4fd741589551d06
nacho <nama1510@gmail.com>
2019-06-04 17:27:36 +0200

    28 - Changing all pages to Pug Format

--- 04-Working-with-Dynamic-Content-&-adding-Templating-Engines/routes/admin.js
+++ 04-Working-with-Dynamic-Content-&-adding-Templating-Engines/routes/admin.js
@@ -1,18 +1,18 @@
 const path = require("path");
 const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router(); //kinda like a mini express app
 
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render("add-product.pug");
 });
 
 router.post("/add-product", (req, res, next) => {
-  products.push({title: req.body.title});
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
