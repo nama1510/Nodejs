8019ae3d744502bc68e41ba241a5ad9623878d93
nacho <nama1510@gmail.com>
2019-06-10 14:17:19 +0200

    31 - MVC setup

---
+++ 05-MVC/routes/admin.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const express = require("express");
+
+const rootDir = require("../util/path");
+
+const router = express.Router(); //kinda like a mini express app
+
+const products = [];
+
+router.get("/add-product", (req, res, next) => {
+  res.render('add-product', {docTitle: "Add Product", path: "/admin/add-product"});
+});
+
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
+});
+
+exports.routes = router;
+exports.products = products;
